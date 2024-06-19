0b70bebbf8df262513308923cc14edb276673b9b
AlvaroRK <adriano.rk06@gmail.com>
2024-06-19 20:11:24 -0300

    carta 1

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function revealMessage() {
+    const message = document.getElementById('message');
+    const btnMessage = document.getElementById('buttonMessage')
+    message.style.display = 'block';
+    btnMessage.style.display = 'none'
+}
